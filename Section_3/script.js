df85a44c5e56b03fc0448d23ab7766992e23de75
mr_lipovsky <pe.lipovsky@gmail.com>
2023-04-18 19:11:09 +0200

    coding chalenge arrays

--- Section_3/script.js
+++ Section_3/script.js
@@ -73,31 +73,78 @@
 // console.log(yearUntilRetaiement(1991, 'Mike'));
 
 // ARRAY
-const friend1 = 'Michal';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+// const friend1 = 'Michal';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
 
-const friends = ['Michal', 'Mike', 'Peter']
+// const friends = ['Michal', 'Mike', 'Peter']
 
+// console.log(friends);
+
+// // const years = new Array(1994, 1995, 1996, 1997, 1998);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay'
+// console.log(friends);
+
+// const name = 'John';
+// const test = [name, 'Jay', 2-1, 'teacher', friends];
+// console.log(test)
+// console.log(test.length);
+
+
+// const calcAge = function (birthYear) {
+//     return 2377 - birthYear;
+// }
+// const years = [1955, 1976, 1977, 1987];
+
+// // console.log(calcAge(years));
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[2]), calcAge(years[years.length -1 ])];
+
+//Add
+const friends = ['Michal', 'Mike', 'Peter'];
+const newLenght = friends.push('Jay'); // add to end of list
+console.log(friends);
+console.log(newLenght);
+
+friends.unshift('John'); // add to beggining list
 console.log(friends);
 
-const years = new Array(1994, 1995, 1996, 1997, 1998);
-
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Jay'
+//Remove
+friends.pop(); // remove last
+const popped = friends.pop(); // just log the popped value 
+console.log(popped); 
 console.log(friends);
 
-const name = 'John';
-const test = [name, 'Jay', 2-1, 'teacher', friends];
-console.log(test)
-console.log(test.length);
+friends.shift(); // remove first
+console.log(friends);
+
+console.log(friends.indexOf('Mike')); // index 
+console.log(friends.indexOf('bob')); // is not in array will be -1
+
+friends.push(23);
+
+console.log(friends.includes('Mike')); // check if is in array True
+console.log(friends.includes('Bob')); // check if is in array False
+console.log(friends.includes('23')); // check number and we looking for string is in array False, if looking for number - True
 
 
-const calcAge = function (birthYear) {
-    return 2377 - birthYear;
+if (friends.includes('Peter')) {
+    console.log('Your firend is call Peter')
 }
+
+
+
+
+
+
