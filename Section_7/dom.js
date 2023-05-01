c989719bb47c112c77adce0d97f532858e10631f
mr_lipovsky <pe.lipovsky@gmail.com>
2023-05-01 20:19:20 +0200

    guess game

--- Section_7/dom.js
+++ Section_7/dom.js
@@ -13,28 +13,73 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNo = Math.trunc(Math.random() * 20) + 1;
-const score = 20;
-document.querySelector('.number').textContent = secretNo;
+let secretNo = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
+    // reset button
+    document.querySelector('.again').addEventListener('click', function() {
+            console.log('again')
+            let secretNo = Math.trunc(Math.random() * 20) + 1;
+            let score = 20;
+            let guess = Number(document.querySelector('.guess').value = null); 
+            document.querySelector('.message').textContent = 'Start guessing...';
+            document.querySelector('.number').textContent = '?';
+            document.querySelector('body').style.backgroundColor = '#222';
+            document.querySelector('.number').style.width = '15rem';
+            document.querySelector('.score').textContent = score;
+    })
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess').value); 
-    console.log(guess, typeof guess); 
-
+    document.querySelector('.check').addEventListener('click', function() {
+        const guess = Number(document.querySelector('.guess').value); 
+  
+    //when there is not imput
     if(!guess) {
-    document.querySelector('.message').textContent = 'No Number ';
+    document.querySelector('.message').textContent = 'No Number';
+    
+    //when player wins
     } else if (guess === secretNo) {
-        document.querySelector('.message').textContent = 'Correct number ';
-    } else if (guess > secretNo) {
-        document.querySelector('.message').textContent = 'Number too high!';
-        Scoreboard ;
-        document.querySelector('.score').textContent = score;
+        document.querySelector('.message').textContent = 'Correct number';
+        document.querySelector('.number').textContent = secretNo;
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.width = '30rem';
 
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highScore').textContent = highScore;
+        }
+
+    //when number is higher than
+    } else if (guess > secretNo) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Number too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('body').style.backgroundColor = 'red';
+        }
+
+    //when number is smaller than
     } else if (guess < secretNo) {
-        document.querySelector('.message').textContent = 'Number too low!'; 
-        score--;
-        document.querySelector('.score').textContent = score;
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Number too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('body').style.backgroundColor = 'red';
+        }
     }
 });
  
+// Coding Challenge #1
+// Implement a game rest functionality, so that the player can make a new guess!
+// Your tasks:
+// 1. Select the element with the 'again' class and attach a click event handler 
+// 2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
+// 3. Restore the initial conditions of the message, number, score and guess input fields
+// 4. Also restore the original background color(#222) and number width(15rem)
+// GOOD LUCK 😀
