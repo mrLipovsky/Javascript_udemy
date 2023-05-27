bf3ff58c359513a36d71ca59d04b61a781422ad1
mr_lipovsky <pe.lipovsky@gmail.com>
2023-05-27 18:24:53 +0200

    chalange

--- Section_10/coding_chalenge.js
+++ Section_10/coding_chalenge.js
@@ -13,9 +13,12 @@
 // (Write option number)
 
 // 1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+
 // 2. Call this method when ever the user clicks the "Answerpoll" button.
+
 // 3. Createamethod'displayResults'whichdisplaysthepollresults.The
 // method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+
 // 4. Runthe'displayResults'methodattheendofeach 'registerNewAnswer' method call.
 
 // 5. Bonus:Usethe'displayResults'methodtodisplaythe2arraysinthetest data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
@@ -28,19 +31,36 @@
 
 const poll = {
     question: 'What is your favourite programming language? \n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++ \n (Write option number)', 
-    options: ["JavaScript", "Python", "Rust", "C++"],
+    options: ["0:JavaScript", "1:Python", "2:Rust", "3:C++"],
     // This generates [0, 0, 0, 0]. More in the next section! 
     answers: new Array(4).fill(0),
     };
-
-const registerNewAnswer = function() {
-    // let answares = prompt(poll.question);
-    console.log(Object.keys(poll.options[3]));
-
-    // if(answares === poll.options[3]) {
-    //     console.log('lipo')
-    // }
-
+    
+document.getElementById('poll').onclick = function(){registerNewAnswer()
 }
 
-console.log(registerNewAnswer())
+const registerNewAnswer = function() {
+    let answares = prompt(poll.question);    
+
+    if(answares === poll.options[0][0]) {
+        poll.answers[0] += 1
+        console.log('lipo')
+    } else if (answares === poll.options[1][0]) {
+        poll.answers[1] += 1
+
+        console.log('lipo1')
+    } else if (answares === poll.options[2][0]) {
+        poll.answers[2] += 1
+
+        console.log('lipo2')
+    } else if (answares === poll.options[3][0]) {
+        poll.answers[3] += 1
+
+        console.log('lipo3')
+    }
+}
+
+console.log(poll.answers)
+
+// console.log(registerNewAnswer())
+
